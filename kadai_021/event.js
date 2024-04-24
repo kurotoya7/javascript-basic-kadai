ca1ad8d405da93c678197b8386db949e42bdedd2
kurotoya7 <161889834+kurotoya7@users.noreply.github.com>
2024-04-24 11:01:53 +0900

    Add files via upload

---
+++ kadai_021/event.js
@@ -0,0 +1,26 @@
+const btn7 =document.getElementById('btn');
+// const text=document.getElementById('text');
+
+setTimeout(() =>{
+  const text=document.getElementById('text');
+  text.textContent="クリックされました!";
+  },2000)
+
+
+// btn7.addEventListener('click',()=>{
+//   text.textContent="クリックされました!";
+
+
+// })
+
+
+
+
+
+// btn7.setTimeout(() => {
+  
+//   btn7.click();
+// }, 2000);
+
+
+
